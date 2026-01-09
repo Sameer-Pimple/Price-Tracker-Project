a15bec61e5d6f50daccbeb3d50cd9fa16c9370f3
Sameer-Pimple <sameerpimple2002@gmail.com>
2026-01-09 18:33:11 +0530

    Add_\Project_From_Local

---
+++ src/main/java/com/pricetracker/Price/Tracker/Project/PriceTrackerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pricetracker.Price.Tracker.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceTrackerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceTrackerProjectApplication.class, args);
+	}
+
+}
