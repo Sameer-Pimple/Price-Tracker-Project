6fc8ce8b784a388382ca2de20afef14fec66a0f3
Sameer-Pimple <sameerpimple2002@gmail.com>
2026-01-09 18:33:11 +0530

    Add_\Project_From_Local

---
+++ src/test/java/com/pricetracker/Price/Tracker/Project/PriceTrackerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pricetracker.Price.Tracker.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceTrackerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
