51b1bf45b5ccd0c09d1f28e0c65e75e84125b3ca
Sameer-Pimple <sameerpimple2002@gmail.com>
2026-02-13 05:37:07 +0530

    MAKING Seprate Folders

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import History from "./pages/History";
+import Landing from "./marketing/Landing";
+import Deals from "./marketing/Deals";
+import Trends from "./marketing/Trends";
+import Alerts from "./marketing/Alerts";
+import AppLayout from "./components/AppLayout";
+import "./styles/theme.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/history/:id" element={<History />} />
+          <Route path="/landing" element={<Home />} />
+          <Route path="/deals" element={<Deals />} />
+          <Route path="/trends" element={<Trends />} />
+          <Route path="/alerts" element={<Alerts />} />
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
+  );
+}
+
+export default App;
